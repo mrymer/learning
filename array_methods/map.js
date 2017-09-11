2701db7460e88145511c70ece3a3db54672a4a49
Maria Rymer <mariarymer@Marias-MacBook-Pro.local>
2017-09-11 14:42:12 -0400

    adding tape function

--- array_methods/map.js
+++ array_methods/map.js
@@ -1,18 +1,18 @@
 //using map on an array and testing it
 
-// var test = require ('tape');
+var test = require ('tape');
 var numbers = [4,9,16,25,36] //array of numbers
-
-// function mapFunction (x){
-//    root = numbers.map(Math.sqrt)
-// }
 
 var root= numbers.map(Math.sqrt)
 console.log(root)
 
-// test('map test', Math.sqrt{
-//   t.plan(1);
+// function mapFunction (){
 //
-//   t.equal(root,'2,3,4,5,6')
-//   var start = numbers.map.(Math.sqrt);
-// })
+// }
+
+test('map test', Math.sqrt{
+  t.plan(1);
+
+  t.equal(root,'2,3,4,5,6')
+  var start = numbers.map.(Math.sqrt);
+})
